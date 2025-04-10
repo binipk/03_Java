8972a23269ff11532d7a1af9c5fa3ba884e59a32
binipk <mingyoku93@gmail.com>
2025-04-10 13:52:18 +0900

    update

--- 02_KYEONGBIN/chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section02/enumtype/Subjects.java
+++ 02_KYEONGBIN/chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section02/enumtype/Subjects.java
@@ -2,22 +2,21 @@ package com.ohgiraffers.section02.enumtype;
 
 public enum Subjects {
 
-    JAVA,
-    MySQL,
-    JDBC,
-    HTML,
-    CSS,
-    JAVASCRIPT;
+    JAVA,   // 0
+    MYSQL,  // 1
+    JDBC,   // 2
+    HTML,   // 3
+    CSS,    // 4
+    JAVASCRIPT; //5
 
     Subjects() {
-        System.out.println("Normal Constructor is called...");
+        System.out.println("기본 생성자 호출됨....");
     }
 
     public String toString() {
         return "@@@@" + this.name() + "@@@@";
     }
 
-    public enum BackEnd {JAVA, MYSQL, JDBC, JAVASCRIPT};
-    public enum FrontEnd {HTML, JAVASCRIPT, CSS};
-
+    public enum BackEnd { JAVA, MYSQL, JDBC, JAVASCRIPT };
+    public enum FrontEnd { HTML, CSS, JAVASCRIPT};
 }
