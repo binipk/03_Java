5350c6ccaea8facf512f9759b4a108ae539aeb42
binipk <mingyoku93@gmail.com>
2025-04-20 23:23:53 +0900

    update

--- 02_KYEONGBIN/00_Practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application3.java
+++ 02_KYEONGBIN/00_Practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application3.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.operatorPractice;
 
+import java.util.Scanner;
+
 public class Application3 {
 
     public static void main(String[] args) {
@@ -14,14 +16,41 @@ public class Application3 {
          * 남자입니다.
          * */
 
-         char gender;
-         gender = 'M';
+         Scanner sc = new Scanner(System.in);
 
-         if (gender == 'M') {
-            System.out.println("남자입니다.");
-         } else if (gender == 'F') {
-            System.out.println("여자입니다.");
+         while(true) {
+            
+            System.out.println("성별을 입력해 주세요! (M/F) \n'q'키를 눌러 종료합니다.");
+
+            String input = sc.nextLine();
+
+            if(input.equalsIgnoreCase("q")) {
+                System.out.println("\nExiting the progream");
+                break;
+            }
+
+            try {
+                if(input.equalsIgnoreCase("m")) {
+                    System.out.println("it's male.");
+                } else if (input.equalsIgnoreCase("f")){
+                    System.out.println("it's female.");
+                } else {
+                    System.out.println("Please enter M' or 'F'");
+                }
+
+
+            } catch (Exception e) {
+            }
+
          }
+         sc.close();
+
+         
+         
+
+
+
+
 
     }
 }
