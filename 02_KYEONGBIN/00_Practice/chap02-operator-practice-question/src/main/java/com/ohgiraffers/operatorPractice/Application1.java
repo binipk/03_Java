377db510b27605abf0b06a180f41f621c741f099
binipk <mingyoku93@gmail.com>
2025-04-26 18:24:37 +0900

    Practice

--- 02_KYEONGBIN/00_Practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application1.java
+++ 02_KYEONGBIN/00_Practice/chap02-operator-practice-question/src/main/java/com/ohgiraffers/operatorPractice/Application1.java
@@ -19,30 +19,36 @@ public class Application1 {
 
         Scanner sc = new Scanner(System.in);
 
-
         while (true) {
-
-            System.out.println("Please enter the number: \nEnter'q' to quit!");
-
+            System.out.println("Enter number: \n(Enter 'q' to quit");
             String input = sc.nextLine();
 
             if (input.equalsIgnoreCase("q")) {
-                System.out.println("Exiting the program!");
+                System.out.println("Exiting program!");
                 break;
             }
 
             try {
                 int num = Integer.parseInt(input);
-                if (num % 2 == 0) {
-                    System.out.println("The number is even!");
+                if (num % 2 == 0 && num > 0) {
+                    System.out.println("The number is even");
+
+                } else if (num % 2 != 0 && num > 0) {
+                    System.out.println("The number is odd");
+
+                } else if (num < 0) {
+                    System.out.println("The number cannot be negative!");
                 } else {
-                    System.out.println("the number is odd!");
+                    System.out.println("Please enter the correct number");
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Your enter is incorrect! please enter again!");
+
+            } catch (Exception e) {
+                System.out.println("Please enter the correct number");
+
             }
         }
         sc.close();
+
 
         
 
